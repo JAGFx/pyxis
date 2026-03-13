343f6f7e65bbfa0c2d8b817fa58ecc4c0e3fd0d3
Emmanuel SMITH <emmanuel.smith@jagfx.fr>
2026-03-13 12:10:10 +0100

    :recycle: (artifact) move compiler pass on export module

--- src/Module/Exporter/ExporterBundle.php
+++ src/Module/Exporter/ExporterBundle.php
@@ -2,8 +2,18 @@
 
 namespace App\Module\Exporter;
 
+use App\Module\Exporter\Infrastructure\RequestExport\Attribute\AsExportCommand;
+use App\Module\Exporter\Infrastructure\RequestExport\DependencyInjection\RequestExportPass;
+use App\Module\Exporter\Infrastructure\RequestExport\Factory\RequestExportCommandFactory;
+use App\Module\Exporter\Infrastructure\RequestExport\Message\Command\RequestExportCommandInterface;
 use Override;
+use ReflectionClass;
+use RuntimeException;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ChildDefinition;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class ExporterBundle extends AbstractBundle
@@ -18,5 +28,38 @@ class ExporterBundle extends AbstractBundle
                     ->defaultTrue()
                     ->info('Enable or disable the Export module')
                 ->end();
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RequestExportPass(), PassConfig::TYPE_BEFORE_REMOVING);
+    }
+
+    /**
+     * @param array<string, mixed> $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $builder->registerAttributeForAutoconfiguration(
+            AsExportCommand::class,
+            static function (
+                ChildDefinition $definition,
+                AsExportCommand $attribute,
+                ReflectionClass $reflector,
+            ): void {
+                if (!$reflector->implementsInterface(RequestExportCommandInterface::class)) {
+                    throw new RuntimeException(sprintf('Class "%s" is tagged with #[AsExportCommand] but does not implement "%s".', $reflector->getName(), RequestExportCommandInterface::class));
+                }
+
+                /** @var RequestExportCommandInterface $instance */
+                $instance = $reflector->newInstance();
+
+                $definition->addTag(RequestExportCommandFactory::DEFINITION_TAG, [
+                    RequestExportCommandFactory::ATTRIBUTE_KEY_NAME => $instance->getName(),
+                ]);
+            }
+        );
     }
 }
